d9ec5d1a7c93f9b16a970e475a3d93c5bd4fabb1
Dheeraj Ahuja <dheeraj.ahuja@sap.com>
2022-06-21 14:05:58 +0530

    beautified

--- src/index.js
+++ src/index.js
@@ -10,8 +10,7 @@ const element = (
       <li>Was first released in 2013</li>
       <li>Was originally created by Jordan Walke</li>
       <li>Has well over 100K stars on GitHub</li>
-      <li>Is maintained by Facebook</li> 
-      <li>Is maintained by Facebook</li>
+      <li>Is maintained by Facebook</li> <li>Is maintained by Facebook</li>
       <li>Powers thousands of enterprise apps, including mobile apps</li>
     </ul>
   </div>
